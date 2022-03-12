da79c9cfc8a0dcf08798fa15a0aeb9ad721bc60b
Alinailka <alina-il@mail.ru>
2022-03-12 23:52:20 +0700

    Working version

--- src/main/java/ru/netology/domein/Radio.java
+++ src/main/java/ru/netology/domein/Radio.java
@@ -2,96 +2,70 @@ package ru.netology.domein;
 
 public class Radio {
 
-    int currentChanel = 8;
-
-
     public int getNewChanel() {
         return currentChanel;
+    }
 
+    private int currentChanel = 5;
 
-        public void setNewChanel(int newChanel){
-
-            public int newChanel () {
-                if (newChanel > 9) {
-                    return;
-                }
-                if (newChanel < 0) {
-                    return;
-                }
+    public void setNewChanel(int newChanel) {
+        if (newChanel >= 0) {
+            if (newChanel <= 9)
                 currentChanel = newChanel;
-            }
-
-
-            public int getNextChanel () {
-                return nextChanel();
-            }
-
-            public void setNextChanel () {
-                this.currentChanel = nextChanel();
-            }
-//    private int nextChanel() {
-//
-//        for (int i = currentChanel; i < 9; i++) {
-//            return i + 1;
-//        }
-//        if (currentChanel == 9) {
-//            return 0;
-//        }
-//        return 0;
-//    }
-            private int nextChanel () {
-
-                if (currentChanel < 9) {
-                    return currentChanel + 1;
-                }
-                if (currentChanel == 9) {
-                    return 0;
-                }
-                if (currentChanel > 9) {
-                    return currentChanel;
-                }
-                return 0;
-            }
-
-
-            public int prevChanel () {
-                for (int i = currentChanel; i > 0; i--) {
-                    return i - 1;
-                }
-                if (currentChanel == 0) {
-                    return 9;
-                }
-                return 0;
-            }
-
-            int currentVolume = 0;
-
-            public int getCurrentVolume () {
-                return currentVolume;
-            }
-
-            public void setCurrentVolume ( int currentVolume){
-                this.currentVolume = currentVolume;
-            }
-
-            public int addVolume () {
-                for (int i = currentVolume; i < 10; i++) {
-                    return i + 1;
-                }
-                if (currentVolume == 10) {
-                    return currentVolume;
-                }
-                return 0;
-            }
-            public int downVolume () {
-                for (int i = currentVolume; i > 0; i--) {
-                    return i - 1;
-                }
-                if (currentVolume == 0) {
-                    return currentVolume;
-                }
-                return 0;
-            }
-
         }
+        if (currentChanel == newChanel) ;
+        if (newChanel < 0) ;
+        if (newChanel > 9) ;
+        return;
+    }
+
+    public int nextChanel(int currentChanel) {
+        if (currentChanel < 9) {
+            if (currentChanel >= 0) ;
+            currentChanel = currentChanel + 1;
+            return currentChanel;
+        }
+        if (currentChanel == 9) {
+            currentChanel = 0;
+            return currentChanel;
+        }
+        return 0;
+    }
+
+    public int prevChanel(int currentChanel) {
+        if (currentChanel > 0) {
+            if (currentChanel <= 9) ;
+            currentChanel = currentChanel - 1;
+            return currentChanel;
+        }
+        if (currentChanel == 0) {
+            currentChanel = 9;
+            return currentChanel;
+        }
+        return 0;
+    }
+
+    public int addVolume(int currentVolume) {
+        if (currentVolume < 10) {
+            if (currentVolume >= 0) ;
+            currentVolume = currentVolume + 1;
+            return currentVolume;
+        }
+        if (currentVolume == 10) {
+            return currentVolume;
+        }
+        return 0;
+    }
+
+    public int downVolume(int currentVolume) {
+        if (currentVolume > 0)
+            if (currentVolume <= 10) {
+                return currentVolume - 1;
+            }
+        if (currentVolume == 0) {
+            return currentVolume;
+        }
+        return 0;
+    }
+}
 
